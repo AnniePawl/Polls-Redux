97aa0a0d89f819040c1bb46e58b7a621c6359bd9
Anna Pawl <annampawl@gmail.com>
2020-04-27 17:39:23 -0400

    fixed app_name to pass signup test

--- registration/urls.py
+++ registration/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-app_name = 'registration'
+app_name = 'registration:signup'
 urlpatterns = [
     path('signup/', views.SignUpView.as_view(), name='signup'),
 
